f10dba93524f7a53e975f1ab539745e5b31505f9
Bogdan Marushkin <nononoksno@gmail.com>
2026-02-16 22:18:44 +0300

    Калькулятор нововведения

--- calculator.py
+++ calculator.py
@@ -1,18 +1,21 @@
 while True:
     print("Введите число, потом знак операции,", end=' ')
-    print("которую хотите совершить (+,-,/,*)", end=' ')
+    print("которую хотите совершить (+,-,/,*,&,//)", end=' ')
+    # '+' - сложение, '-' - вычетание, '/' - обычное деление
+    # '//' - целочисленное деление, '*' - умножение, '&' - остаток
+
     print("и затем второе число")
     print("Чтобы выключить программу введите 'q'")
     
-    num1 = input()
+    num1 = input("Введите первое число: ")
     if num1 == 'q':
         break
     
-    calculate = input()
+    calculate = input("Введите операцию: ")
     if calculate == 'q':
         break
     
-    num2 = input()
+    num2 = input("Введите второе число:")
     if num2 == 'q':
         break
     
@@ -25,11 +28,15 @@ while True:
             answer = int(num1) * int(num2)
         elif calculate == '/':
             answer = int(num1) // int(num2)
+        elif calculate == '&':
+            answer = int(num1) & int(num2)
+        elif calculate == '/':
+            answer = int(num1) / int(num2)
         else:
             print("Неподдерживаемая операция!")
             continue
             
-        print(answer)
+        print(f"Результат: {answer}")
         
     except ValueError:
         print("Вы ввели не число!")
